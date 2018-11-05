15a14ebd9b9dc6d2720529f8f6c6c75f33aed945
Cameron Soulette <csoulette@mustard.prism>
2018-11-05 13:42:30 -0800

    added script to append drimseq pvalues to jb table

---
+++ appendToJBTable.sh
@@ -0,0 +1,18 @@
+FILE1=$1
+FILE2=$2
+
+
+if [ -z "$FILE1" ]
+then
+   echo "Missing FILE1 argument. Correct usage syntax: bash appendToJBTable.sh FILE1 (drim_out.tsv) FILE2 (amended_jb_Table.tsv)"
+   exit 1
+fi
+
+if [ -z "$FILE2" ]
+then
+   echo "Missing FILE2 argument. Correct usage syntax: bash appendToJBTable.sh FILE1 (drim_out.tsv) FILE2 (amended_jb_Table.tsv)"
+   exit 1
+fi
+
+
+awk 'FNR==NR{a[$2]=$(NF-1)"\t"$NF; next} {if ($1 in a)print $0,a[$1]}' $FILE1 $FILE2
